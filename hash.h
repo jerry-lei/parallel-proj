369e808c638fdb16cca21b9d6e1473c06aa8e7a4
Jerry Lei <jerrylei98@gmail.com>
2018-04-18 17:17:06 -0400

    Deleted unused code.

--- hash.h
+++ hash.h
@@ -6,11 +6,6 @@
 #include <stdint.h>
 #include <inttypes.h>
 
-struct color_hash{
-  uint64_t red;
-  uint64_t green;
-  uint64_t blue;
-};
 
 struct hsv_hash{
   uint64_t h;
@@ -22,17 +17,6 @@ struct hsv_hash{
 };
 
 int hamming_distance(uint64_t* hash1, uint64_t* hash2);
-void * thread_hash_gray(void * args);
-void * thread_hash_color(void * args);
-void split_hash_gray(struct board** search_image,uint64_t** original_hashed_image, int original_dim_x, int original_dim_y);
-void split_hash_color(struct board** search_image,struct color_hash** original_hashed_image, int original_dim_x, int original_dim_y);
-uint64_t** hash_original_gray(struct board** original_image,int* original_dim_x, int* original_dim_y);
-struct color_hash** hash_original_color(struct board** original_image,int* original_dim_x, int* original_dim_y);
-uint64_t hash8_gray_board(struct board** board);
-uint64_t hash8_gray_pixels(struct pixel** board, int start_x, int start_y);
-struct color_hash hash8_color_pixels(struct pixel** board, int start_x, int start_y);
-
-
 
 
 void * thread_hash_HSV(void * args);
